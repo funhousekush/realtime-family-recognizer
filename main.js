38282af4e8071b4e92e33df80f1d351fbc5436f2
funhousekush <85101915+funhousekush@users.noreply.github.com>
2021-07-07 16:23:42 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,19 @@ function setup()
 {
     canvas = createCanvas(300, 300);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide()
+
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/xTkje456L/model.json", modelLoaded);
+}
+
+function modelLoaded()
+{
+    console.log("model is loaded")
+}
+
+function draw()
+{
+    image(video, 0, 0, 300, 300);
 }
